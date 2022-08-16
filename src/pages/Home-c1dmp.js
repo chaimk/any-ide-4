62f3168efa70349308a686e6e11d91ad4432c3fe
Velo <105860237+velo-app[bot]@users.noreply.github.com>
2022-08-16 13:24:00 +0000

    Initial commit

---
+++ src/pages/Home-c1dmp.js
@@ -0,0 +1,5 @@
+$w.onReady(function () {
+	$w('#button1').onClick( () => {
+		$w('#text3').text = "You clicked the button, you moron!"
+	})
+});
---
+++ src/pages/Random page just for fun-r9wlo.js
@@ -0,0 +1,7 @@
+$w.onReady(function () {
+	// Write your JavaScript here
+
+	// To select an element by ID use: $w('#elementID')
+
+	// Click 'Preview' to run your code
+});
