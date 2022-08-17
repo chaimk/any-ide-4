e48d15a8b23030fe6babe05098be4a970681ff87
chaimk <chaimk@wix.com>
2022-08-17 18:30:58 +0300

    yet another dummy commit

--- src/pages/Home-c1dmp.js
+++ src/pages/Home-c1dmp.js
@@ -1,6 +1,7 @@
 $w.onReady(function () {
 	$w('#button1').onClick( () => {
 		console.log("goodbye")
+		console.log("and good riddance")
 		$w('#text3').html = `<h1 style="color:maroon;font-size:30px;background-color:yellow"><b>You clicked the button, you moron!</b></h2>`
 	})
 });
