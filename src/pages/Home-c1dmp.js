327892e093f4866587cd98eb7704960f304edb2b
chaimk <chaimk@wix.com>
2022-08-17 19:13:15 +0300

    :pray:

--- src/pages/Home-c1dmp.js
+++ src/pages/Home-c1dmp.js
@@ -2,6 +2,7 @@ $w.onReady(function () {
 	$w('#button1').onClick( () => {
 		console.log("goodbye")
 		console.log("and good riddance")
+		console.log("third time's the charm? 🤷🏻‍♂️")
 		$w('#text3').html = `<h1 style="color:maroon;font-size:30px;background-color:yellow"><b>You clicked the button, you moron!</b></h2>`
 	})
 });
