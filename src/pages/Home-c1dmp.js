f99cf8231a18d65aff8ae700a8972c3f00bf3db0
chaimk <chaimk@wix.com>
2022-08-17 13:14:39 +0300

    Dummy console.log

--- src/pages/Home-c1dmp.js
+++ src/pages/Home-c1dmp.js
@@ -1,5 +1,6 @@
 $w.onReady(function () {
 	$w('#button1').onClick( () => {
+		console.log("hello")
 		$w('#text3').html = `<h1 style="color:maroon;font-size:30px;background-color:yellow"><b>You clicked the button, you moron!</b></h2>`
 	})
 });
