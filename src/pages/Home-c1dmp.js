728282786173fdb1263aa0067c73874374cf8b70
chaimk <chaimk@wix.com>
2022-08-17 13:00:20 +0300

    It works! With colors! 😉

--- src/pages/Home-c1dmp.js
+++ src/pages/Home-c1dmp.js
@@ -1,5 +1,5 @@
 $w.onReady(function () {
 	$w('#button1').onClick( () => {
-		$w('#text3').text = "You clicked the button, you moron!"
+		$w('#text3').html = `<h1 style="color:maroon;font-size:30px;background-color:yellow"><b>You clicked the button, you moron!</b></h2>`
 	})
 });
