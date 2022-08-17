c5a5c3a8014ec7cd81c5559b359337ced1f1c0fe
chaimk <chaimk@wix.com>
2022-08-17 13:15:34 +0300

    'Nother meaningless commit, and why not?

--- src/pages/Home-c1dmp.js
+++ src/pages/Home-c1dmp.js
@@ -1,6 +1,6 @@
 $w.onReady(function () {
 	$w('#button1').onClick( () => {
-		console.log("hello")
+		console.log("goodbye")
 		$w('#text3').html = `<h1 style="color:maroon;font-size:30px;background-color:yellow"><b>You clicked the button, you moron!</b></h2>`
 	})
 });
